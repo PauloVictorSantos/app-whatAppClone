78a0c3be68d75f87428a04ee20faa70ba9a75a3a
paulovictorln <paulovictor494@gmail.com>
2018-09-24 20:30:30 -0300

    implementando o combine reducers

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import {Provider}  from 'react-redux';
-import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Rotas from './src/Rotas';
+import reducers from './src/reducers/index';
 
 export default class App extends Component {
   render() {
