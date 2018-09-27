1a9c14f5acf78ad33589189e62c5bbbd46a566c5
paulovictorln <paulovictor494@gmail.com>
2018-09-27 03:24:56 -0300

    FormCadastro - Alterando os estados do componente via ActionCreators - parte 1

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { View, TextInput, Button } from 'react-native';
+import { modificaEmail, modificaSenha } from '../Actions/AutenticacaoActions';
+import { connect } from 'react-redux';
 
 const formCadastro = props => {
     console.log(props);
     return (
         <View style={{ flex: 1, padding: 10 }}>
             <View style={{ flex: 4, justifyContent: 'center' }}>
-                <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }} />
-                <TextInput value={props.email} placeholder="E-mail" style={{ fontSize: 20, height: 45 }} />
-                <TextInput value={props.senhas} placeholder="Senha" style={{ fontSize: 20, height: 45 }} />
+                <TextInput value={props.nome}
+                    placeholder="Nome" style={{ fontSize: 20, height: 45 }} />
+                <TextInput value={props.email}
+                    onChangeText={texto => props.modificaEmail(texto)}
+                    placeholder="E-mail" style={{ fontSize: 20, height: 45 }} />
+                <TextInput value={props.senhas}
+                    onChangeText={texto => props.modificaSenha(texto)}
+                    placeholder="Senha" style={{ fontSize: 20, height: 45 }} />
             </View>
             <View style={{ flex: 1 }}>
                 <Button title="Cadastrar" color="#115E54" onPress={() => false} />
@@ -23,4 +30,4 @@ const mapStateToProps = state => ({
     senha: state.AutenticacaoReducer.senha
 });
 
-export default connect(mapStateToProps, null)(formCadastro);
+export default connect(mapStateToProps, {modificaEmail, modificaSenha})(formCadastro);
