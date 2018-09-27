c4b2ca47d98360938254e208958c25eabdbfb153
paulovictorln <paulovictor494@gmail.com>
2018-09-27 15:55:02 -0300

    Conectando o WhatsApp Clone ao Firebase

--- App.js
+++ App.js
@@ -5,7 +5,21 @@ import { createStore } from 'redux';
 import Rotas from './src/Rotas';
 import reducers from './src/reducers/index';
 
+import firebase from 'firebase';
+
 export default class App extends Component {
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyAFn-kqs3bYBW251w6HrGZBvtuRTOfqzQk",
+      authDomain: "whatappclone-ac5c1.firebaseapp.com",
+      databaseURL: "https://whatappclone-ac5c1.firebaseio.com",
+      projectId: "whatappclone-ac5c1",
+      storageBucket: "whatappclone-ac5c1.appspot.com",
+      messagingSenderId: "269692871941"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
