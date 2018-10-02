1e98408fd7eb6fb66a86f3b2dd6014dc88fea511
paulovictorln <paulovictor494@gmail.com>
2018-10-02 14:25:29 -0300

    Firebase - Adicionando os dados de contatos

--- src/Actions/AutenticacaoActions.js
+++ src/Actions/AutenticacaoActions.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase';
-import {Actions} from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
+import b64  from 'base-64';;
+
 
 export const modificaEmail = (texto) => {
     console.log(texto);
@@ -26,17 +28,22 @@ export const modificaNome = (texto) => {
 export const cadastraUsuario = ({ nome, email, senha }) => {
     return dispatch => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
-            .then(user => cadastroUsuarioSucesso(dispatch))
+            .then(user => {
+                let emailb64 = b64.encode(email);
+                
+                firebase.database().ref("/contatos/"+emailb64)
+                .push({ nome})
+                .then(value=>cadastroUsuarioSucesso(dispatch))
+
+            })
             .catch(erro => cadastroUsuarioErro(erro, dispatch));
     }
 }
 
 const cadastroUsuarioSucesso = (dispatch) => {
-    dispatch(
-        {
-            type: 'cadastro_usuario_sucesso'
-        }
-    );
+    dispatch({
+        type: 'cadastro_usuario_sucesso'
+    });
     Actions.boasVindas();
 }
 
