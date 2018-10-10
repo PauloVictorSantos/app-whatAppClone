41572a44e608db15bd678221983221f437bc0b01
paulovictorln <paulovictor494@gmail.com>
2018-10-10 20:11:21 -0300

    . Iniciando conversas parte 4 - Controlando o campo mensagem com Redux

--- src/components/Conversa.js
+++ src/components/Conversa.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput, TouchableHighlight } from 'react-native';
 
 export default class Conversa extends Component {
     render() {
         return (
-            <View>
-                <Text>Conversa</Text>
+            <View style={{ flex: 1, marginTop: 50, backgroundColor: '#eee4dc', padding: 10 }}>
+                <View style={{ flex: 1, paddingBottom: 20 }}></View>
+                <View style={{ flexDirection: 'row', height: 60 }}>
+                    <TextInput
+                        style={{ flex: 4, backgroundColor: '#fff', fontSize: 18 }}
+                    />
+                </View>
+                <TouchableHighlight onPress={() => false} underlayColor='#fff'>
+                    <Image source={require('../imgs/enviar_mensagem.png')} />
+                </TouchableHighlight>
             </View>
         );
 
