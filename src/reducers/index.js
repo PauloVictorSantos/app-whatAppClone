8d6c13c4b7bf651ce061107a52bff77e8ce71756
paulovictorln <paulovictor494@gmail.com>
2018-10-10 16:37:53 -0300

    correções

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import AutenticacaoReducers from './AutenticacaoReducer';
 import AppReducer from './AppReducer';
+import ListaContatosReducers from './ListaContatosReducers';
 
 export default combineReducers({
     AutenticacaoReducers,
