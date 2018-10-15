e1cab5878081ebc2d0c20c3c137757c955493fec
paulovictorln <paulovictor494@gmail.com>
2018-10-14 21:59:13 -0300

    Trocando mensagens parte 3 - Recuperando mensagens

--- src/components/Conversa.js
+++ src/components/Conversa.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { View, TextInput, TouchableHighlight, Image } from 'react-native';
 import { connect } from 'react-redux';
-import { modificaMensagem, enviarMensagem } from '../Actions/AppActions';
+import _ from 'lodash';
+import { modificaMensagem, enviarMensagem, conversaUsuarioFetch } from '../Actions/AppActions';
 
 export class Conversa extends Component {
 
-    _enviarMensagem(){
-        const {mensagem, contatoNome, contatoEmail}  =  this.props;
+    componentWillMount() {
+        this.props.conversaUsuarioFetch(this.props.contatoEmail);
+    }
+
+    _enviarMensagem() {
+        const { mensagem, contatoNome, contatoEmail } = this.props;
         this.props.enviarMensagem(mensagem, contatoNome, contatoEmail);
     }
 
     render() {
-        console.log("props de conversa",this.props);
         return (
             <View style={{ flex: 1, marginTop: 50, backgroundColor: '#eee4dc', padding: 10 }}>
                 <View style={{ flex: 1, paddingBottom: 20 }}></View>
@@ -33,9 +37,17 @@ export class Conversa extends Component {
 }
 
 mapStateToProps = state => {
+
+    const conversa = _.map(state.ListaConversaReducer,(val, uid)=>{
+        return {...state, uid};
+    });
+
+    console.log(conversa);
+
     return ({
+        conversa,
         mensagem: state.AppReducer.mensagem
     })
 }
 
-export default connect(mapStateToProps, { modificaMensagem, enviarMensagem })(Conversa);
+export default connect(mapStateToProps, { modificaMensagem, enviarMensagem, conversaUsuarioFetch })(Conversa);
